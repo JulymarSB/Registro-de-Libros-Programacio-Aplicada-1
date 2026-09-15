38831e873fdb4d8769c1168fa9add19ad178b0e6
Julymar Santos Balbi <julymar_santos@ucne.edu.do>
2026-09-14 21:28:02 -0400

    Practica en Clase

---
+++ RegistroLibros/Context/Contexto.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using RegistroLibros.Models;
+
+namespace RegistroLibros.Context;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto>options) : base(options){}
+
+    public DbSet<Libros> Libros {get; set;}
+}
---
+++ RegistroLibros/Services/LibroService.cs
@@ -0,0 +1,67 @@
+using System.Linq.Expressions;
+using Aplicada1.Core;
+using Microsoft.EntityFrameworkCore;
+using RegistroLibros.Context;
+using RegistroLibros.Models;
+using Aplicada1.Core;
+namespace RegistroLibros.Services;
+
+public class LibroService(IDbContextFactory<Contexto> DbFactory): IService<Libros, int>
+{
+    public async Task<bool> ExisteLibro(String titulo)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros.AnyAsync(l =>l.Titulo.ToLower() == titulo.ToLower());
+        
+    }
+
+    public async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros.AnyAsync(l => l.LibroId == id);
+    }
+
+    public async Task<bool> Insertar(Libros libro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Libros.Add(libro);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Modificar(Libros libro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Libros.Update(libro);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Guardar(Libros libro)
+    {
+        if(libro.LibroId == 0)
+        {
+            return await Insertar(libro);
+        }
+        else
+        {
+            return await Modificar(libro);
+        }
+    }
+
+    public async Task<Libros?> Buscar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros.FirstOrDefaultAsync(l => l.LibroId ==  id);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros.Where(l => l.LibroId == id).ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<List<Libros>> GetList(Expression<Func<Libros, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros.Where(criterio).AsNoTracking().ToListAsync();
+    }
+}
